08b04379ad4dad627d2de178b2934a5c2af4bb38
Jorge Gomez <jorge3257685@gmail.com>
2022-01-27 19:22:31 -0800

    Changed text font and alignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+
+let dogpic = document.querySelectorAll('.dog-image')
+for (let i=0; i<dogpic.length; i++){
+dogpic[i].style.borderStyle = 'solid';
+dogpic[i].style.borderRadius = '4px';
+dogpic[i].style.borderColor = 'black';
+}
+
+
+let doginfo = document.querySelectorAll('.dog-description');
+for (let i=0; i<doginfo.length; i++){
+    doginfo[i].style.fontFamily = 'Georgia'
+    }
+
+let dogn = document.querySelectorAll('.dog-name');
+for (let i=0; i<dogn.length; i++){
+    dogn[i].style.textAlign  = 'left';
+    }
+
+let foot = document.querySelector('.footer');
+foot.style.Color = 'red';
 
 // Select some elements...
